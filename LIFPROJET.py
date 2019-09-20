a3bccdc158e6f4761f2827727efa5e4e264adca8
AlexisPollonni <alexis.pollonni@gmail.com>
2019-09-20 16:37:59 +0200

    create createBox function.

--- LIFPROJET.py
+++ LIFPROJET.py
@@ -1,16 +1,39 @@
 import pybullet as p
 import time
 import pybullet_data
+
+def createBox(boxHalfLength, boxHalfWidth, boxHalfHeight):
+	boxID = p.createVisualShape(p.GEOM_BOX, halfExtents=[boxHalfLength, boxHalfWidth, boxHalfHeight])
+	colID = p.createCollisionShape(p.GEOM_BOX, halfExtents=[boxHalfLength, boxHalfWidth, boxHalfHeight])
+
+	return p.createMultiBody(baseMass=1,
+                      baseInertialFramePosition=[0, 0, 0],
+                      baseCollisionShapeIndex=colID,
+                      baseVisualShapeIndex=boxID,
+                      basePosition=[0, 0, 1],
+                      useMaximalCoordinates=True)
+	
+
+
+
 physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 p.setGravity(0,0,-10)
 planeId = p.loadURDF("plane.urdf")
-cubeStartPos = [0,0,1]
-cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
-boxId = p.loadURDF("r2d2.urdf",cubeStartPos, cubeStartOrientation)
+
+
+
+
+boxId = createBox(1,1,2)
+
+
+
+
+
 for i in range (10000):
     p.stepSimulation()
     time.sleep(1./240.)
+
 cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
 print(cubePos,cubeOrn)
 p.disconnect()
