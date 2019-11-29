4c7bcb1124340560a16ca45445cf50e227597b10
johanna trost <johanna.trost@insa-lyon.fr>
2019-11-29 15:58:30 +0100

    nettoyage de branch

--- gwendal_tests/no_snake.py
+++
@@ -1,8 +0,0 @@
-import pybullet as p
-import pybullet_data
-import time
-from urdfEditor import *
-
-boxId = p.loadURDF("test.urdf",[5,5,5], .getQuaternionFromEuler([0,0,0]))
-cubePos, cubeOrn = p.getBasePositionAndOrientation(boxId)
-print(cubePos,cubeOrn)
